6f6816d142b05f52f1692581cb3618c715ae4b17
Bayu Siddhi Mukti <bayu2mukti18@gmail.com>
2024-07-13 00:47:46 +0700

    Add str encode to EmbeddingModel

--- simple_RAG/embedding_model.py
+++ simple_RAG/embedding_model.py
@@ -14,12 +14,15 @@ class EmbeddingModel:
             device=self.device
         )
 
-    def encode(self, filtered_chunks: list[dict], device: str = None) -> Embedding:
+    def encode(self, filtered_chunks_or_query: list[dict] | str, device: str = None) -> Embedding:
         self.device = self.device if device is None else Helper.set_device(device)
         self.model.to(self.device)
-        for item in filtered_chunks:
-            item['embedding'] = self.model.encode(item['chunk'])
-            # item['embedding'] = self.model.encode(item['chunk'], convert_to_tensor=True)
+        if isinstance(filtered_chunks_or_query, list):
+            for item in filtered_chunks_or_query:
+                item['embedding'] = self.model.encode(item['chunk'])
+        elif isinstance(filtered_chunks_or_query, str):
+            filtered_chunks_or_query = self.model.encode(filtered_chunks_or_query, convert_to_tensor=True)
+
         embedding: Embedding = Embedding()
-        embedding.add_new(filtered_chunks)
+        embedding.add_new(filtered_chunks_or_query)
         return embedding
