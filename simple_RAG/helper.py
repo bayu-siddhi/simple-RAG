c90ee53a5711fec613626afc363ae5010050b27e
Bayu Siddhi Mukti <bayu2mukti18@gmail.com>
2024-08-11 12:53:17 +0700

    Delete unused parameters

--- simple_RAG/helper.py
+++ simple_RAG/helper.py
@@ -8,7 +8,7 @@ class Helper:
         return text.replace("\n", " ").strip()
 
     @staticmethod
-    def set_device(device: str, from_class: str, print_status: bool = True) -> str:
+    def set_device(device: str, from_class: str) -> str:
         device_used = 'cuda' if device == 'cuda' and torch.cuda.is_available() else 'cpu'
         if device == device_used:
             print(f"[INFO] {device} is available, using {device_used} as the {from_class} device")
